fd7fc102c3ab188f9bbbc0e500a7792bf84910a6
Revan Sarbast <revansarbast8@gmail.com>
2020-07-03 08:43:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+
+function updateDriverWithKeyAndValue(driver='sam', key, value){
+  
+  const newObj = { ...driver };
+  newObj[key]=value
+  return newObj
+}
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+  driver[key] = value;
+ 
+  return driver;
+}
+function deleteFromDriverByKey(driver, key){
+  
+}
+function destructivelyDeleteFromDriverByKey(driver, key){
+  
+}
+console.log(updateDriverWithKeyAndValue({name:"sam"}, "addres", "value"))
